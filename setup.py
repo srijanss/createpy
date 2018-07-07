0554397c6a61056b002d2595d3cf293778690919
Srijan Manandhar <srijan.manandhar@gmail.com>
2018-07-07 14:34:44 +1000

    create templates folder python package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='createpyproject',
-    version='0.0.8',
+    version='0.0.9',
     author="Srijan Manandhar",
     author_email="srijan.manandhar@gmail.com",
     description='Test project to use python script for creating barebone python project folder structure',
@@ -23,18 +23,18 @@ setuptools.setup(
         'jinja2',
     ],
     python_requires=">=2.7, >=3.0",
-    package_data={
-        'createpyproject': [
-            'example_templates/example_README.md',
-            'example_templates/example_LICENSE.txt',
-            'example_templates/example_setup.py',
-            'example_templates/.gitignore',
-            'example_templates/.travis.yml',
-            'example_templates/Makefile',
-            'example_templates/licenses.json'
-        ]
-    },
-    include_package_data=True,
+    # package_data={
+    #     'createpyproject': [
+    #         'example_templates/example_README.md',
+    #         'example_templates/example_LICENSE.txt',
+    #         'example_templates/example_setup.py',
+    #         'example_templates/.gitignore',
+    #         'example_templates/.travis.yml',
+    #         'example_templates/Makefile',
+    #         'example_templates/licenses.json'
+    #     ]
+    # },
+    # include_package_data=True,
     entry_points='''
     [console_scripts]
     createpyproject=createpy.cli:main
