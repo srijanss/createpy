94931b396323187f06bf7d6b23ebe2170f6eb88c
Srijan Manandhar <srijan.manandhar@gmail.com>
2018-07-07 12:37:49 +1000

    Changed project name to avoid collision

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='createpy',
+    name='createpyproject',
     version='0.0.2',
     author="Srijan Manandhar",
     author_email="srijan.manandhar@gmail.com",
